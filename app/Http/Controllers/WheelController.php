38c71581481b9c859f1bd7c71ad9e0737d47907b
wbconnor <wmconnor1986@gmail.com>
2018-10-18 09:47:16 -0400

    Eager load brand name with wheels

--- app/Http/Controllers/WheelController.php
+++ app/Http/Controllers/WheelController.php
@@ -14,9 +14,7 @@ class WheelController extends Controller
      */
     public function index()
     {
-        //$wheels = Wheel::withBrandNames()->all();
-
-        $wheels = Wheel::all();
+        $wheels = Wheel::with(['brand:id,name'])->get();
 
         $columns = [
           'id',
