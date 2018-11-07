87ef987b0aebc008317105037618695eea455f35
wbconnor <wmconnor1986@gmail.com>
2018-11-07 15:22:38 -0500

    Create pictures migration

---
+++ database/migrations/2018_03_17_003404_pictures.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Pictures extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url');
+            $table->boolean('cover');
+            $table->integer('wheel_id')->unsigned();
+            $table->foreign('wheel_id')->references('id')->on('wheels');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pictures');
+    }
+}
